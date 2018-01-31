ba85de09c61d11e4b2e3f5f96018dbaf4171dbd7
kennest <kennyoulai@gmail.com>
2018-01-31 17:16:59 +0000

    starter

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,240 +1,197 @@
 <!doctype html>
 <html>
-
 <head lang="fr">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-
 	<title>WAKA - Opérateur internet</title>
 	<meta name="description" content="Waka .sa - opérateur internet" />
 	<meta name="keywords" content="opérateur internet, afrique, haut-débit, waka, Gabon, Côte d'ivoire,communication" />
 	<meta property="og:title" content="" />
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
 	/>
-
-	<!--link rel="stylesheet" type="text/css" href="css/style.css"-->
-	<link rel="stylesheet" type="text/css" href="css/mega-menu.css">
+	<!--link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/mega-menu.css"-->
 	<link rel="stylesheet" type="text/css" href="css/chat-box.css">
 	<link rel="stylesheet" type="text/css" href="wow/dist/animate.css">
-
 	<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,600,300,200&subset=latin,latin-ext' rel='stylesheet'
 	 type='text/css' />
 	<style>
-		.navbar-static-top {
-			margin-bottom: 0px;
+		#first-nav {
+			height: 50px;
+		}
+		#first-nav>div>div>ul>li>a {
+			color: white;
+		}
+		#first-nav>div>div>ul>li>a:hover{
+			color:#41aa46;
+		}
+		#second-nav,
+		#third-nav {
+			background-color: white;
+		}
+		#third-nav {
+			border-top: 1px solid #8d9598;
+		}
+		#second-nav>div>div>nav>ul>li>a {
+			color: #3b4042;
+			padding: 5px;
+		}
+		#second-nav > div > div:nth-child(4){
+			top:35px;
+		}
+		#third-nav > div > div > div > ul > li.nav-item > a{
+			color:#53585d;
+		}
+		#third-nav > div > div > div:nth-child(3) > form > a{
+			color:#41aa46;
+		}
+		#third-nav > div > div > div:nth-child(3) > form > a:hover{
+			color:white;
+		}
+		#slide{
+			margin-top:200px;
 		}
 	</style>
 </head>
-
 <body>
-	<div class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">
-					<img class="img-rounded" alt="Logo" src="images/logo.jpg" height="50">
-				</a>
+	<header class="fixed-top">
+		<nav id="first-nav" class="container-fluid navbar navbar-inverse bg-primary">
+			<div class="row">
+				<div class="col-1">&nbsp;</div>
+				<div class="col">
+					<ul class="nav list-inline">
+						<li>
+							<a href=""><i class="fa fa-2x fa-facebook-official"></i></a>
+						</li>
+						<li>&nbsp;</li>
+						<li>&nbsp;</li>
+						<li>
+							<a href=""><i class="fa fa-2x fa-twitter-square"></i></a>
+						</li>
+						<li>&nbsp;</li>
+						<li>&nbsp;</li>
+						<li>
+							<a href=""><i class="fa fa-2x fa-linkedin-square"></i></a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-6">
+					&nbsp;
+				</div>
+				<div class="col">
+					<ul class="nav list-inline">
+						<li>
+							<a href=""><i class="fa fa-envelope"></i>Email : infos@waka-sa.ga</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-1">&nbsp;</div>
 			</div>
-			<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li>
-						<a href="#">ACCUEIL</a>
-					</li>
-					<li class="dropdown menu-large">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">SOLUTIONS ET SERVICES
-							<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu megamenu row">
-							<li class="col-sm-4">
-								<ul>
-									<li class="dropdown-header text-uppercase">
-										<h5>
-											Solution de connectivité haut-débit
-										</h5>
-									</li>
-									<li class="divider"></li>
-									<li>
-										<a href="#">Fibre Optique Pro</a>
-									</li>
-									<li>
-										<a href="#">Vsat Pro</a>
-									</li>
-									<li>
-										<a href="#">VPN</a>
-									</li>
-									<li>
-										<a href="#">Wimax</a>
-									</li>
-									<li>
-										<a href="#">Interconnection sites distants</a>
-									</li>
-								</ul>
+			<hr/>
+		</nav>
+		<nav id="second-nav" class="navbar bg-light container-fluid">
+			<div class="row">
+				<div class="col-1">&nbsp;</div>
+				<div class="col">
+					<img src="{{asset('images/logo.jpg')}}" height="100" width="170" class="rounded">
+				</div>
+				<div class="col-5">&nbsp;</div>
+				<div class="col">
+					<nav class="nav list-inline">
+						<ul class="nav list-inline">
+							<li>
+								<a href="#">A propos</a>
 							</li>
-							<li class="col-sm-4">
-								<ul>
-									<li class="dropdown-header text-uppercase">
-										<h5>
-											Solutions à valeurs ajoutée
-										</h5>
-									</li>
-									<li class="divider"></li>
-									<li>
-										<a href="#">Voip</a>
-									</li>
-									<li>
-										<a href="#">Solution Ip Tv</a>
-									</li>
-									<li>
-										<a href="#">Video conférence</a>
-									</li>
-									<li>
-										<a href="#">Software et plateforme As a service</a>
-									</li>
-									<li>
-										<a href="#">Vidéo surveillance</a>
-									</li>
-								</ul>
+							<li>|</li>
+							<li>
+								<a href="#">Solutions Waka</a>
 							</li>
-							<li class="col-sm-4">
-								<ul>
-									<li class="dropdown-header text-uppercase">
-										<h5>
-											Etude et développement
-										</h5>
-									</li>
-									<li class="divider"></li>
-									<li>
-										<a href="#">Conception d'archtectures</a>
-									</li>
-									<li>
-										<a href="#">Supervision</a>
-									</li>
-								</ul>
+							<li>|</li>
+							<li>
+								<a href="#">Blog</a>
+							</li>
+							<li>|</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+							<li>&nbsp;</li>
+							<li>
+								<a href="#">
+										<i class="fa fa-question-circle"></i>
+								</a>
 							</li>
 						</ul>
-					</li>
-					<li class="dropdown menu-large">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">A PROPOS
-							<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu megamenu row">
-							<li class="col-sm-4 col-sm-offset-4">
-								<ul>
-									<li class="dropdown-header text-uppercase">
-										<h5>
-											Qui sommes nous ? Les équipes
-										</h5>
-									</li>
-									<li class="divider"></li>
-									<li>
-										<a href="#">présentation des valeurs de l'entreprise</a>
-									</li>
-									<li>
-										<a href="#">Mise en avant de la qualité de service, charte qualité</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-					<li class="dropdown menu-large">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">BLOG
-							<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu megamenu row">
-							<li class="col-sm-4 col-sm-offset-5">
-								<ul>
-									<li class="dropdown-header text-uppercase">
-										<h5>
-											Actualité technologie et entreprise veille
-										</h5>
-									</li>
-									<li class="divider"></li>
-									<li>
-										<a href="#">L'actualité WAKA</a>
-									</li>
-									<li>
-										<a href="#">événements</a>
-									</li>
-									<li>
-										<a href="#">Solutions telecoms</a>
-									</li>
-									<li>
-										<a href="#">témoignages et avis</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-					<li class="dropdown menu-large">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">ESPACE CLIENT
-							<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu megamenu row">
-							<li class="col-sm-3 col-sm-offset-3">
-								<ul>
-									<li>
-										<a href="#">
-											Newsletter
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											Base de données client
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											FAQ
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											Devis online
-										</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a href="#">CONTACT</a>
-					</li>
-				</ul>
-				<ul class="nav navbar-nav">
-					<li>
-						<a href="">&nbsp;</a>
-					</li>
-				</ul>
-				<ul class="nav navbar-nav">
-					<li>
-						<a href="">
-							<i class="fa fa-facebook"></i>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<i class="fa fa-twitter"></i>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<i class="fa fa-linkedin"></i>
-						</a>
-					</li>
-				</ul>
+					</nav>
+				</div>
 			</div>
-		</div>
-	</div>
-
-	<!-- Chat Box-->
+		</nav>
+		<nav id="third-nav" class="navbar bg-faded">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<ul class="nav list inline">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Fibre <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">VSAT</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">VOIP</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">WIMAX</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">VPN</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-6">
+						&nbsp;
+					</div>
+					<div class="col">
+						<form class="form-inline my-2 my-lg-0">
+							<a href="#" class="btn btn-outline-success">Login</a>
+						</form>
+					</div>
+				</div>
+			</div>
+			</div>
+		</nav>
+	</header>
+	<section id="slide">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+					  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+					  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+					  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner" role="listbox">
+					  <div class="carousel-item active">
+						<img class="d-block img-fluid" src="http://via.placeholder.com/1900x550" alt="First slide">
+					  </div>
+					  <div class="carousel-item">
+						<img class="d-block img-fluid" src="http://via.placeholder.com/1900x550" alt="Second slide">
+					  </div>
+					  <div class="carousel-item">
+						<img class="d-block img-fluid" src="http://via.placeholder.com/1900x550" alt="Third slide">
+					  </div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					  <span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					  <span class="sr-only">Next</span>
+					</a>
+				  </div>
+	</section>
+	<!-- Chat Box>
 	<div id="chatbox" role="tablist">
-		<div class="panel panel-success">
+		<div class="panel panel-info">
 			<div class="panel-heading" role="tab" id="headingOne">
 				<h4 class="panel-title">
 					<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -272,142 +229,103 @@
 			</div>
 		</div>
 	</div>
-	<!-- Chat Box-->
-
-
+	<Chat Box-->
 	<!-- === MAIN Background === -->
-	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<img src="http://via.placeholder.com/1900x650" alt="...">
-				<div class="carousel-caption">
-					<h3>One</h3>
-					<p>First Slide</p>
-				</div>
-			</div>
-			<div class="item">
-				<img src="http://via.placeholder.com/1900x650" alt="...">
-				<div class="carousel-caption">
-					<h3>Two</h3>
-					<p>Second Slide</p>
-				</div>
-			</div>
-			<div class="item">
-				<img src="http://via.placeholder.com/1900x650" alt="...">
-				<div class="carousel-caption">
-					<h3>Three</h3>
-					<p>Third Slide</p>
-				</div>
-			</div>
-
-			<!-- Controls -->
-			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
-	</div>
-
 	@yield('content')
-
 	<footer class="footer">
 		<nav class="navbar navbar-default navbar-static-top">
 			<div class="container">
 				<p>&nbsp;</p>
-					<div class="row">
-						<div class="col-lg-3">
-							<img src="images/logo.jpg" height="150" width="200" class="img-rounded" alt="">
-							<p class="lead">
-									Opérateur télécom - Expert dans la gestion de réseaux de télécommunications et 
-									la confidentialité des données - 20 ans d’expérience.
-							</p>
-						</div>
-						<div class="col-lg-2">
-							<h4>DIVERS</h4>
-							<ul class="nav navbar-nav list-unstyled">
-								<li>
-									<a href="#">Partenaires</a>
-								</li>
-								<li>
-									<a href="#">Blog</a>
-								</li>
-								<li>
-									<a href="#">Plan du site</a>
-								</li>
-								<li>
-									<a href="#">Mentions légales</a>
-								</li>
-								<li>
-									<a href="#">Conditions</a>
-								</li>
-								<li>
-									<a href="#">FAQ</a>
-								</li>
-							</ul>
-						</div>
-						<div class="col-lg-2">
-							<h4>CONTACT</h4>
-							<ul class="nav navbar-nav">
-								<li>
-									<a href="#">Adresse</a>
-								</li>
-								<li>
-									<ul class="nav navbar-nav">
-										<li>
-											<a href="">
-												<i class="fa fa-facebook"></i>
-											</a>
-										</li>
-										<li>
-											<a href="">
-												<i class="fa fa-twitter"></i>
-											</a>
-										</li>
-										<li>
-											<a href="">
-												<i class="fa fa-linkedin"></i>
-											</a>
-										</li>
-									</ul>
-								</li>
-							</ul>
-						</div>
-						<div class="col-lg-3">
-							<h4>SOLUTIONS ET SERVICES</h4>
-						</div>
+				<div class="row">
+					<div class="col-3">
+						<img src="images/logo.jpg" height="150" width="200" class="img-rounded" alt="">
+						<p class="lead">
+							Opérateur télécom - Expert dans la gestion de réseaux de télécommunications et la confidentialité des données - 20 ans d’expérience.
+						</p>
+					</div>
+					<div class="col-3">
+						<h4>SOLUTIONS ET SERVICES</h4>
+						<ul class="nav navbar-nav list-unstyled">
+							<li>
+								<a href="#">Solution connectivité haut-débit</a>
+							</li>
+							<li>
+								<a href="#">Solutions à valeurs ajoutée</a>
+							</li>
+							<li>
+								<a href="#">Etude de développement</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-2">
+						<h4>DIVERS</h4>
+						<ul class="nav navbar-nav list-unstyled">
+							<li>
+								<a href="#">Partenaires</a>
+							</li>
+							<li>
+								<a href="#">Blog</a>
+							</li>
+							<li>
+								<a href="#">Plan du site</a>
+							</li>
+							<li>
+								<a href="#">Mentions légales</a>
+							</li>
+							<li>
+								<a href="#">Conditions</a>
+							</li>
+							<li>
+								<a href="#">FAQ</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-2">
+						<h4>CONTACT</h4>
+						<ul class="nav navbar-nav">
+							<li>
+								<a href="#">Adresse</a>
+							</li>
+							<li>
+								<a href="#">Numero de Telephone</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-6">
+			<div class="container-fluid">
+				<div class="row" id="copyright">
+					<div class="col-sm-6 col-sm-offset-4">
 						<h4><b>Copyright ©2018 {{env('APP_NAME')}}. All rights reserved.</b></h4>
 					</div>
-					<div class="col-sm-4 col-sm-offset-2">
-
+					<div class="col-sm-2">
+						<ul class="nav navbar-nav">
+							<li>
+								<a href="">
+												<i class="fa fa-facebook"></i>
+											</a>
+							</li>
+							<li>
+								<a href="">
+												<i class="fa fa-twitter"></i>
+											</a>
+							</li>
+							<li>
+								<a href="">
+												<i class="fa fa-linkedin"></i>
+											</a>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
 		</nav>
 	</footer>
-
 	<!-- SCRIPTS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
 	<script src="wow/dist/wow.min.js"></script>
 	<script src="js/megamenu.js"></script>
-
 	<script src="https://js.pusher.com/4.1/pusher.min.js"></script>
 	<script>
 		wow = new WOW(
@@ -422,17 +340,14 @@
                     wow.init();
 		// Enable pusher logging - don't include this in production
         Pusher.logToConsole = true;
-
         var pusher = new Pusher('9849132e1f27d01ac2fc', {
             cluster: 'eu',
             encrypted: true
         });
-
         var channel = pusher.subscribe('my-channel');
         channel.bind('my-event', function(data) {
             alert(data.message);
         });
 	</script>
 </body>
-
 </html>
